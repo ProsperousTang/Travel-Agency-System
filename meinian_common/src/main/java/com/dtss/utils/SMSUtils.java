836b10893d2cc258a04c13fb5d32790ab46593bb
ProsperousTang <75344970+ProsperousTang@users.noreply.github.com>
2020-12-03 07:01:11 +0000

    Update SMSUtils.java

--- meinian_common/src/main/java/com/dtss/utils/SMSUtils.java
+++ meinian_common/src/main/java/com/dtss/utils/SMSUtils.java
@@ -30,8 +30,8 @@ public class SMSUtils {
      *          3XFb3eNq6IDZ5Ik3x10vo9YHHzMGBz
      *
      *
-     *          LTAI4FxugG9NkQ2DFAD1TT7X
-     *          H8dhZjvulQLdrLwmbRrePVfZPhQRPJ
+     *          
+     *          
      */
     public static void sendShortMessage(String phoneNumbers,String param) throws Exception{
         // 设置超时时间-可自行调整
@@ -42,8 +42,8 @@ public class SMSUtils {
         final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
 
         // 替换成你的AK
-        final String accessKeyId = "LTAI4FxugG9NkQ2DFAD1TT7X";// 你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "H8dhZjvulQLdrLwmbRrePVfZPhQRPJ";// 你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "LTAI4Fbws3CCEzKx3JPHqXQC";// 你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "3XFb3eNq6IDZ5Ik3x10vo9YHHzMGBz";// 你的accessKeySecret，参考本文档步骤2
         // 初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
         DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
